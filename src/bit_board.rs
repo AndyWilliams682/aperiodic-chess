46757e8687ecc76b25b23decd6d5017409c24155
Andy Williams <andywilliams682@gmail.com>
2024-10-21 22:19:32 -0700

    Extending functionality

--- src/bit_board.rs
+++ src/bit_board.rs
@@ -1,4 +1,5 @@
 use petgraph::graph::{Graph, NodeIndex};
+use std::collections::HashSet;
 
 use crate::piece::Piece;
 
@@ -7,20 +8,10 @@ use crate::piece::Piece;
 pub struct BitBoard(u64);
 
 impl BitBoard {
-    pub fn from_node_indices(node_indices: Vec<NodeIndex>) -> BitBoard {
+    pub fn from_node_indices(node_indices: HashSet<NodeIndex>) -> BitBoard {
         let mut output: u64 = 0;
         for node in node_indices {
             output += 1 << node.index();
-        }
-        return BitBoard(output)
-    }
-
-    pub fn get_knight_movement_bbs(num_directions: i32, graph: Graph<Piece, i32>) -> BitBoard {
-        let mut output: u64 = 0;
-        for node in graph.node_indices() {
-            for direction in 0..num_directions {
-                
-            }
         }
         return BitBoard(output)
     }
@@ -34,7 +25,7 @@ mod tests {
     #[test]
     fn test_generate() {
         assert_eq!(
-            BitBoard::from_node_indices(vec![NodeIndex::new(0), NodeIndex::new(25)]),
+            BitBoard::from_node_indices(HashSet::from_iter([NodeIndex::new(0), NodeIndex::new(25)])),
             BitBoard(33554433)
         )
     }
