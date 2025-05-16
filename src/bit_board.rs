726ef028f932d786acc7d7bb8f59e1c61efd11a2
Andy Williams <andywilliams682@gmail.com>
2025-05-15 23:53:42 -0700

    Added BitBoardMoves object to act as an iterator over a bitboard, treating all 1s as possible moves to be made. Handles pawns as a special case

--- src/bit_board.rs
+++ src/bit_board.rs
@@ -2,6 +2,9 @@ use petgraph::graph::NodeIndex;
 use std::collections::HashSet;
 use std::ops::{Sub, BitAnd, BitOr, Not};
 
+use crate::position::PieceType;
+use crate::chess_move::{EnPassantData, Move};
+
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub struct BitBoard(u128);
@@ -121,6 +124,71 @@ impl Iterator for CarryRippler {
     }
 }
 
+#[derive(Debug)]
+pub struct BitBoardMoves {
+    source_node: NodeIndex,
+    piece_type: PieceType,
+    remaining_moves: BitBoard,
+    next_ep_data: Option<EnPassantData>,
+    promotable_nodes: Option<Vec<NodeIndex>>,
+    current_promotion_values: Vec<PieceType>
+}
+
+impl BitBoardMoves {
+    pub fn new(source_node: NodeIndex, piece_type: PieceType, remaining_moves: BitBoard, next_ep_data: Option<EnPassantData>, promotable_nodes: Option<Vec<NodeIndex>>) -> BitBoardMoves {
+        BitBoardMoves { source_node, piece_type, remaining_moves, next_ep_data, promotable_nodes, current_promotion_values: vec![] }
+    }
+}
+
+impl Iterator for BitBoardMoves {
+    type Item = Move;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(to_node) = self.remaining_moves.lowest_one() {
+            let mut promotion = None;
+            let mut en_passant_tile = None;
+            if self.piece_type == PieceType::Pawn {
+                match &self.next_ep_data {
+                    Some(data) if data.piece_tile == to_node => {
+                        en_passant_tile = Some(data.capturable_tile)
+                    },
+                    _ => {}
+                }
+
+                match &self.promotable_nodes {
+                    Some(nodes) if nodes.contains(&to_node) => {
+                        promotion = match self.current_promotion_values.len() {
+                            0 => {
+                                self.current_promotion_values.push(PieceType::Knight);
+                                Some(PieceType::Knight)
+                            },
+                            1 => {
+                                self.current_promotion_values.push(PieceType::Bishop);
+                                Some(PieceType::Bishop)
+                            },
+                            2 => {
+                                self.current_promotion_values.push(PieceType::Rook);
+                                Some(PieceType::Rook)
+                            },
+                            _ => {
+                                self.current_promotion_values = vec![];
+                                self.remaining_moves.flip_bit_at_node(to_node);
+                                Some(PieceType::Queen)
+                            }
+                        };
+                    },
+                    _ => { self.remaining_moves.flip_bit_at_node(to_node); }
+                }
+            } else {
+                self.remaining_moves.flip_bit_at_node(to_node);
+            }
+            Some(Move::new(self.source_node, to_node, promotion, en_passant_tile))
+        } else {
+            None
+        }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -204,5 +272,116 @@ mod tests {
             test.next(),
             None
         )
+    }
+
+    #[test]
+    fn test_bitboard_moves_knight() {
+        let source_node = NodeIndex::new(0);
+        let piece_type = PieceType::Knight;
+        let remaining_moves = BitBoard::from_ints(vec![10, 17]);
+        let mut bitboard_moves = BitBoardMoves::new(
+            source_node, piece_type, remaining_moves, None, None
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(10), None, None)
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(17), None, None)
+        );
+        assert_eq!(
+            bitboard_moves.next(),
+            None
+        );
+    }
+
+    #[test]
+    fn test_bitboard_moves_rook() {
+        let source_node = NodeIndex::new(63);
+        let piece_type = PieceType::Rook;
+        let remaining_moves = BitBoard::from_ints(vec![60, 61, 62]);
+        let mut bitboard_moves = BitBoardMoves::new(
+            source_node, piece_type, remaining_moves, None, None
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(60), None, None)
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(61), None, None)
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(62), None, None)
+        );
+        assert_eq!(
+            bitboard_moves.next(),
+            None
+        );
+    }
+
+    #[test]
+    fn test_bitboard_moves_pawn_no_promotion() {
+        let source_node = NodeIndex::new(8);
+        let piece_type = PieceType::Pawn;
+        let remaining_moves = BitBoard::from_ints(vec![16, 17, 24]);
+        let en_passant_data = Some(EnPassantData { 
+            capturable_tile: NodeIndex::new(16),
+            piece_tile: NodeIndex::new(24) 
+        });
+        let mut bitboard_moves = BitBoardMoves::new(
+            source_node, piece_type, remaining_moves, en_passant_data, None
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(16), None, None)
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(17), None, None)
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(24), None, Some(NodeIndex::new(16)))
+        );
+        assert_eq!(
+            bitboard_moves.next(),
+            None
+        );
+    }
+
+    #[test]
+    fn test_bitboard_moves_pawn_with_promotion() {
+        let source_node = NodeIndex::new(48);
+        let piece_type = PieceType::Pawn;
+        let remaining_moves = BitBoard::from_ints(vec![56, 57]);
+        let mut bitboard_moves = BitBoardMoves::new(
+            source_node, piece_type, remaining_moves, None, Some(vec![
+                NodeIndex::new(56),
+                NodeIndex::new(57)
+            ])
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(56), Some(PieceType::Knight), None)
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(56), Some(PieceType::Bishop), None)
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(56), Some(PieceType::Rook), None)
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(56), Some(PieceType::Queen), None)
+        );
+        assert_eq!(
+            bitboard_moves.next().unwrap(),
+            Move::new(source_node, NodeIndex::new(57), Some(PieceType::Knight), None)
+        );
     }
 }
