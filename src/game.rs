45abf17efd934c514ad5beaa9976ecba2069323b
Andy Williams <andywilliams682@gmail.com>
2025-08-26 22:42:38 -0700

    Added some functions to make the UI work

--- src/game.rs
+++ src/game.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use crate::{chess_move::Move, engine::Engine, graph_boards::graph_board::TileIndex, graph_boards::traditional_board::TraditionalBoardGraph, piece_set::Piece, position::{GameOver, Position}};
+use crate::{bit_board::{BitBoard, BitBoardTiles}, chess_move::Move, engine::Engine, graph_boards::{graph_board::{Tile, TileIndex}, traditional_board::TraditionalBoardGraph}, piece_set::{Color, Piece}, position::{GameOver, Position}};
 
 
 
@@ -12,6 +12,16 @@ pub struct Game {
 }
 
 impl Game {
+    pub fn is_over(&mut self) -> Option<GameOver> {
+        if self.current_position.is_checkmate(&self.engine.move_tables) {
+            return Some(GameOver::Checkmate)
+        } else if self.current_position.is_stalemate(&self.engine.move_tables) || self.current_position.fifty_move_draw() { // TODO: Add more draw conditions here
+            return Some(GameOver::Draw)
+        } else {
+            None
+        }
+    }
+
     pub fn play_game(&mut self) {
         let mut turn_count = 0;
         while self.current_position.is_over(&self.engine.move_tables) == None {
@@ -40,6 +50,110 @@ impl Game {
             GameOver::Draw => println!("Game ended in a draw!"),
             GameOver::Checkmate => println!("{} wins by checkmate!", self.current_position.active_player.opponent())
         }
+    }
+
+    pub fn make_cpu_move(&mut self) {
+        let cpu_move = self.engine.search_for_move(&mut self.current_position);
+        self.current_position.make_legal_move(&cpu_move);
+    }
+
+    pub fn query_tile(&mut self, tile_index: &TileIndex) -> BitBoard {
+        let white_pieces = &self.current_position.pieces[0];
+        let black_pieces = &self.current_position.pieces[1];
+        let occupied = white_pieces.occupied | black_pieces.occupied; // TODO: Occupied stored somewhere??
+        
+        let selected_white = white_pieces.get_piece_at(*tile_index); // TODO: These should all be references in functions!
+        let selected_black = black_pieces.get_piece_at(*tile_index);
+        let selected_piece = selected_white.or(selected_black);
+        
+        let (selected_color, allied_occupied, enemy_occupied, pawn_tables) = match black_pieces.get_piece_at(*tile_index) {
+            Some(_t) => (Color::Black, black_pieces.occupied, white_pieces.occupied, &self.engine.move_tables.black_pawn_tables),
+            _ => (Color::White, white_pieces.occupied, black_pieces.occupied, &self.engine.move_tables.white_pawn_tables)
+        };
+
+        let mut pseudo_moves =  match selected_piece {
+            Some(Piece::Pawn) => {
+                self.engine.move_tables.query_pawn(&selected_color, *tile_index, &enemy_occupied, occupied, &self.current_position.record.en_passant_data)
+            },
+            None => BitBoard::empty(),
+            _ => { // All non-Pawn PieceTypes
+                self.engine.move_tables.query_piece(&selected_piece.unwrap(), *tile_index, occupied) & !allied_occupied
+            }
+        };
+
+        // TODO: Playable move is breaking on pawn promotion
+        // Need to make the move a promotion if applicable
+        // If pawn, if destination_tile == a promotion tile, set promotion = Queen
+
+        for destination_tile in BitBoardTiles::new(pseudo_moves) {
+            let mut promotion: Option<Piece> = None;
+            if pawn_tables.promotion_board.get_bit_at_tile(destination_tile) && selected_piece == Some(Piece::Pawn) {
+                promotion = Some(Piece::Queen);
+            }
+            // TODO: Redesign and use BitBoardMoves
+            let chess_move = Move::new(*tile_index, destination_tile, promotion, None);
+            if !self.current_position.is_playable_move(&chess_move, &self.engine.move_tables) {
+                pseudo_moves.flip_bit_at_tile_index(destination_tile);
+            }
+        }
+
+        return pseudo_moves
+    }
+
+    pub fn attempt_move_input(&mut self, source_tile: TileIndex, destination_tile: TileIndex) -> Result<(), TileParseError> {
+        let chess_move = self.parse_move_input(source_tile, destination_tile)?;
+        match self.current_position.is_playable_move(&chess_move, &self.engine.move_tables) {
+            true => {
+                self.current_position.make_legal_move(&chess_move);
+                return Ok(())
+            },
+            false => return Err(TileParseError::InvalidMoveError)
+        }
+    }
+
+    // TODO: Rename equivalent things to source_tile and destination_tile
+    fn parse_move_input(&self, source_tile: TileIndex, destination_tile: TileIndex) -> Result<Move, TileParseError> {
+        // Assumes destination is valid due to limiting the selectable tiles
+        let active_pieces = &self.current_position.pieces[self.current_position.active_player.as_idx()];
+
+        let en_passant_data = match active_pieces.get_piece_at(source_tile) {
+            Some(Piece::Pawn) => {
+                self.engine.move_tables.white_pawn_tables.en_passant_table[source_tile.index()].clone().or(
+                    self.engine.move_tables.black_pawn_tables.en_passant_table[source_tile.index()].clone()
+                )
+            },
+            None => return Err(TileParseError::InvalidMoveError), // Source could be enemy pieces
+            _ => None
+        };
+
+        let en_passant_data = match en_passant_data {
+            Some(epd) => {
+                if epd.occupied_tile != destination_tile {
+                    None
+                } else {
+                    Some(epd)
+                }
+            },
+            None => None
+        };
+
+        let mut promotion = None;
+        if active_pieces.get_piece_at(source_tile) == Some(Piece::Pawn) {
+            let promotion_board = match self.current_position.active_player.as_idx() {
+                0 => self.engine.move_tables.white_pawn_tables.promotion_board,
+                _ => self.engine.move_tables.black_pawn_tables.promotion_board
+            };
+            if promotion_board.get_bit_at_tile(destination_tile) {
+                promotion = Some(Piece::Queen)
+            }
+        }
+        
+        return Ok(Move::from_input(
+            source_tile,
+            destination_tile,
+            promotion,
+            en_passant_data
+        ))
     }
 
     fn get_human_move(&mut self) -> Move {
@@ -134,7 +248,8 @@ impl Game {
 #[derive(Debug, Clone, PartialEq)]
 pub enum TileParseError {
     ParseIntError,
-    TileOutOfRangeError
+    TileOutOfRangeError,
+    InvalidMoveError
 }
 
 #[derive(Debug, Clone, PartialEq)]
